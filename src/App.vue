7bcc91f1fa068cca43d3935d938b6df2c91c4cf8
ike <2740262535@qq.com>
2020-11-03 17:02:30 +0800

    wrong

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
+  body{
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+    font-family: $font-hei;
+    line-height: 1.5;
+  }
+</style>
